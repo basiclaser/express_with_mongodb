426ec40bfb13a7dff73b8df30b7b429df3cc20d8
Christopher Shelley <dont@forgetmy.email>
2022-11-04 11:09:49 +0100

    add cors middleware

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import "dotenv/config"
 import express from "express"
 import connectToDatabase from "./src/database"
 import studentsRouter from "./src/student/router"
+import cors from "cors"
 
 const PORT = process.env.PORT || 3000
 const api = express()
+api.use(cors())
 api.use(express.json())
 
 
